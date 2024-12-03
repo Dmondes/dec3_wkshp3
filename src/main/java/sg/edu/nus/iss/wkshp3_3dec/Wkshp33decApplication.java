9baad632e15b02e84cd2575f6eff96bda2386d40
Dmondes <desmondlimws@gmail.com>
2024-12-03 11:07:44 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/wkshp3_3dec/Wkshp33decApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.wkshp3_3dec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wkshp33decApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wkshp33decApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/wkshp3_3dec/Wkshp33decApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.wkshp3_3dec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wkshp33decApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
